ea0291511e9c04b8ec093cd1e0598f65ae2fb4b6
Docs Robot <robot@example.com>
2023-02-14 15:44:56 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "PCF85063A Arduino Library", "index.html", [
-    [ "Real time clock RTC PCF85063A breakout Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "Real time clock RTC PCF85063A breakout Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
